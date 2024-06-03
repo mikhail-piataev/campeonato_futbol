aa2caa9b3b30d2ba742fddcff26030e2ffb13afb
mikhail-piataev <mikhail.piataev@davinci.edu.ar>
2024-06-03 10:50:54 -0300

    Validaciones estan agregados

--- futbol/src/futbol/Persona.java
+++ futbol/src/futbol/Persona.java
@@ -9,6 +9,9 @@ public class Persona {
 	private String nombre;
 	private String apellido;
 	private LocalDate fechaNacimiento;
+	private static int maxEdad = 110;
+	private static int minEdad = 10;
+	private static int[] diasEnMes = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
 	public Persona(String nombre, String apellido, LocalDate fechaNacimiento) {
 		this.nombre = nombre;
@@ -64,10 +67,22 @@ public class Persona {
 	public static Persona crearManualmente() {
     	String nombre=JOptionPane.showInputDialog(null, "Ingrese el nombre");
     	String apellido=JOptionPane.showInputDialog(null, "Ingrese el apellido");
-    	int ano =Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el año de nacimiento "));
-    	int mes =Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el mes de nacimiento "));
-    	int dia =Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el dia de nacimiento "));
+    	int anoActual=LocalDate.now().getYear();
+    	int ano=ingreseManualmente("el año de nacimiento", anoActual-maxEdad, anoActual-minEdad);
+    	int mes=ingreseManualmente("el mes de nacimiento", 1, 12); 
+    	int dia=ingreseManualmente("el dia de nacimiento", 1, diasEnMes[mes-1]);
     	LocalDate fecha = LocalDate.of(ano, mes, dia);
     	return new Persona(nombre, apellido, fecha);
+	}
+	
+	private static int ingreseManualmente(String texto, int minValor, int maxValor) {
+		int numero;
+		do {
+	       	 numero =Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese " + texto));
+	       	 if (numero < minValor || numero > maxValor) {
+	       		JOptionPane.showMessageDialog(null, texto + " es incorrecto. Ingrese una vez más.");
+	       	 }
+	    	} while (numero < minValor || numero > maxValor);
+		return numero;
 	}
 }
